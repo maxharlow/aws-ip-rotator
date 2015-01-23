c420f18f368f831a377b5847c58976df50804511
Max Harlow <maxharlow@gmail.com>
2015-01-23 17:10:43 +0000

    Nicer output

--- aws-ip-rotator.sh
+++ aws-ip-rotator.sh
@@ -8,11 +8,13 @@ case $1 in
     'stop' )
 	crontab -l | grep -v $(basename $0) | crontab - ;;
     *)
-	INSTANCE=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-	OLD_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+	INSTANCE=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+	OLD_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 	NEW_IP=$(aws ec2 allocate-address --query PublicIp | tr -d '"')
+	echo ''
 	echo "Old IP: $OLD_IP"
 	echo "New IP: $NEW_IP"
+	echo ''
 	echo 'Associating new IP...'
 	aws ec2 associate-address --instance-id $INSTANCE --public-ip $NEW_IP
 	echo 'Releasing old IP...'
