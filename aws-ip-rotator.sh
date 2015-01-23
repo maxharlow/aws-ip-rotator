59f2af954d33bf6fbed59cf0df18d6feaacce51b
Max Harlow <maxharlow@gmail.com>
2015-01-23 16:10:18 +0000

    Let's rotate the board

---
+++ aws-ip-rotator.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+case $1 in
+    'start' )
+	apt-get install -y awscli
+	SCHEDULE='*/10 * * * *' # every ten minutes
+	(crontab -l 2> /dev/null; echo "$SCHEDULE $(pwd)/$(basename $0)") | crontab - ;;
+    'stop' )
+	crontab -l | grep -v $(basename $0) | crontab - ;;
+    *)
+	INSTANCE=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+	OLD_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+	NEW_IP=$(aws ec2 allocate-address --query PublicIp | tr -d '"')
+	echo "Old IP: $OLD_IP"
+	echo "New IP: $NEW_IP"
+	echo 'Associating new IP...'
+	aws ec2 associate-address --instance-id $INSTANCE --public-ip $NEW_IP
+	echo 'Releasing old IP...'
+	aws ec2 release-address --public-ip $OLD_IP
+esac
